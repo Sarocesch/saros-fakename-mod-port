e31f14fc6da565c27d3b8886372ad40d6ebe6a73
Sarocesch <Sarocesch@outlook.de>
2025-12-29 22:07:13 +0100

    Added 1.21.1 Forge Support

--- src/main/java/tschipp/fakename/FakeName.java
+++ src/main/java/tschipp/fakename/FakeName.java
@@ -9,6 +9,7 @@ import net.minecraft.commands.synchronization.SingletonArgumentInfo;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -17,9 +18,10 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.forgespi.language.IModInfo;
-import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.ChannelBuilder;
+import net.minecraftforge.network.NetworkDirection;
 import net.minecraftforge.network.PacketDistributor;
-import net.minecraftforge.network.simple.SimpleChannel;
+import net.minecraftforge.network.SimpleChannel;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -29,30 +31,31 @@ import org.apache.logging.log4j.Logger;
 
 @EventBusSubscriber(bus = Bus.MOD)
 @Mod(FakeName.MODID)
-public class FakeName
-{
+public class FakeName {
     public static final String MODID = "fakename";
 
     public static SimpleChannel network;
 
     public static IModInfo info;
 
-    private static final DeferredRegister<ArgumentTypeInfo<?, ?>> COMMAND_ARGUMENT_TYPES = DeferredRegister.create(ForgeRegistries.COMMAND_ARGUMENT_TYPES, MODID);
-    private static final RegistryObject<SingletonArgumentInfo<FakenameArgumentType>> FAKENAME_ARGUMENT = COMMAND_ARGUMENT_TYPES.register("fakename", () -> {
-        return ArgumentTypeInfos.registerByClass(FakenameArgumentType.class, SingletonArgumentInfo.contextFree(FakenameArgumentType::fakename));
-    });
-    
-    
-    
-    public FakeName()
-    {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    private static final DeferredRegister<ArgumentTypeInfo<?, ?>> COMMAND_ARGUMENT_TYPES = DeferredRegister
+            .create(ForgeRegistries.COMMAND_ARGUMENT_TYPES, MODID);
+    private static final RegistryObject<SingletonArgumentInfo<FakenameArgumentType>> FAKENAME_ARGUMENT = COMMAND_ARGUMENT_TYPES
+            .register("fakename", () -> {
+                return ArgumentTypeInfos.registerByClass(FakenameArgumentType.class,
+                        SingletonArgumentInfo.contextFree(FakenameArgumentType::fakename));
+            });
+
+    @SuppressWarnings("removal")
+    public FakeName(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+        modEventBus.addListener(this::setup);
 
         ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SERVER_SPEC);
 
         info = ModLoadingContext.get().getActiveContainer().getModInfo();
-        
-        COMMAND_ARGUMENT_TYPES.register(FMLJavaModLoadingContext.get().getModEventBus());
+
+        COMMAND_ARGUMENT_TYPES.register(modEventBus);
 
         Logger logger = LogManager.getLogger(MODID);
         InputStream in = FakeName.class.getClassLoader().getResourceAsStream("fakename.mixins.json");
@@ -73,37 +76,39 @@ public class FakeName
         }
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
+    private void setup(final FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
-        	 FakeName.network = NetworkRegistry.newSimpleChannel(new ResourceLocation(FakeName.MODID, "fakenamechannel"), () -> FakeName.info.getVersion().toString(), s -> true, s -> true);
-             FakeName.network.registerMessage(0, FakeNamePacket.class, FakeNamePacket::toBytes, FakeNamePacket::new, FakeNamePacket::handle);        
+            FakeName.network = ChannelBuilder
+                    .named(ResourceLocation.fromNamespaceAndPath(FakeName.MODID, "fakenamechannel"))
+                    .serverAcceptedVersions((status, version) -> true)
+                    .clientAcceptedVersions((status, version) -> true)
+                    .networkProtocolVersion(1)
+                    .simpleChannel();
+
+            FakeName.network.messageBuilder(FakeNamePacket.class, NetworkDirection.PLAY_TO_CLIENT)
+                    .encoder(FakeNamePacket::toBytes)
+                    .decoder(FakeNamePacket::new)
+                    .consumerMainThread(FakeNamePacket::handle)
+                    .add();
         });
-       
+
     }
 
-    public static void sendPacket(Player player, String fakename, int operation)
-    {
+    public static void sendPacket(Player player, String fakename, int operation) {
         performFakenameOperation(player, fakename, operation);
-        FakeName.network.send(PacketDistributor.ALL.noArg(), new FakeNamePacket(fakename, player.getId(), operation));
+        FakeName.network.send(new FakeNamePacket(fakename, player.getId(), operation), PacketDistributor.ALL.noArg());
     }
 
-    public static void performFakenameOperation(Player player, String fakename, int operation)
-    {
+    public static void performFakenameOperation(Player player, String fakename, int operation) {
         CompoundTag tag = player.getPersistentData();
 
-        if (operation == 0)
-        {
+        if (operation == 0) {
             tag.putString("fakename", fakename);
             player.refreshDisplayName();
-        }
-         else
-         {
+        } else {
             tag.remove("fakename");
             player.refreshDisplayName();
         }
     }
-
-
 
 }
--- src/main/java/tschipp/fakename/FakeNamePacket.java
+++ src/main/java/tschipp/fakename/FakeNamePacket.java
@@ -1,65 +1,56 @@
 package tschipp.fakename;
 
-import java.util.function.Supplier;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.network.NetworkEvent.Context;
+import net.minecraftforge.event.network.CustomPayloadEvent;
 
-public class FakeNamePacket
-{
+public class FakeNamePacket {
     public String fakename;
     public int entityId;
     public int deleteFakename;
 
-    public FakeNamePacket(FriendlyByteBuf buf)
-    {
+    public FakeNamePacket(FriendlyByteBuf buf) {
         this.fakename = buf.readUtf();
         this.entityId = buf.readInt();
         this.deleteFakename = buf.readInt();
     }
 
-    public FakeNamePacket()
-    {
+    public FakeNamePacket() {
 
     }
 
-    public FakeNamePacket(String fakename, int entityID, int delete)
-    {
+    public FakeNamePacket(String fakename, int entityID, int delete) {
         this.fakename = fakename;
         this.entityId = entityID;
         this.deleteFakename = delete;
     }
 
-    public void toBytes(FriendlyByteBuf buf)
-    {
+    public void toBytes(FriendlyByteBuf buf) {
         buf.writeUtf(fakename);
         buf.writeInt(entityId);
         buf.writeInt(deleteFakename);
     }
 
-    public void handle(Supplier<Context> ctx)
-    {
-        ctx.get().enqueueWork(() -> {
-        	Minecraft mc = Minecraft.getInstance();
+    public void handle(CustomPayloadEvent.Context ctx) {
+        // Since we use consumerMainThread, work is already enqueued on the main thread
+        Minecraft mc = Minecraft.getInstance();
 
-            Player toSync = (Player) mc.level.getEntity(entityId);
+        Player toSync = (Player) mc.level.getEntity(entityId);
 
-            if (toSync != null)
-            {
-                ctx.get().setPacketHandled(true);
+        if (toSync != null) {
+            FakeName.performFakenameOperation(toSync, fakename, deleteFakename);
 
-                FakeName.performFakenameOperation(toSync, fakename, deleteFakename);
+            if (deleteFakename == 0)
+                mc.player.connection.getPlayerInfo(toSync.getGameProfile().getId())
+                        .setTabListDisplayName(Component.literal(fakename));
+            else
+                mc.player.connection.getPlayerInfo(toSync.getGameProfile().getId())
+                        .setTabListDisplayName(Component.literal(toSync.getGameProfile().getName()));
+        }
 
-                if(deleteFakename == 0)
-                    mc.player.connection.getPlayerInfo(toSync.getGameProfile().getId()).setTabListDisplayName(Component.literal(fakename));
-                else
-                    mc.player.connection.getPlayerInfo(toSync.getGameProfile().getId()).setTabListDisplayName(Component.literal(toSync.getGameProfile().getName()));
-            }
-
-        });
+        ctx.setPacketHandled(true);
     }
 
 }
