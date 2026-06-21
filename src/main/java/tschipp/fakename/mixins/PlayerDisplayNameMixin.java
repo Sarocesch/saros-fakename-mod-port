f6f06ca83811bf2938345e05e766c4196bcbd8ad
Sarocesch <Sarocesch@outlook.de>
2026-06-21 04:47:52 +0200

    port: Saro's FakeName to MC 26.2 (Fabric, Fabric API 0.152.2+26.2) - bump v1.6 -> v1.7

--- src/main/java/tschipp/fakename/mixins/PlayerDisplayNameMixin.java
+++ src/main/java/tschipp/fakename/mixins/PlayerDisplayNameMixin.java
@@ -1,8 +1,8 @@
 package tschipp.fakename.mixins;
 
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.text.Text;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,17 +13,17 @@ import tschipp.fakename.FakeNameData;
  * Mixin to override player display name when a fakename is set.
  * This replaces Forge's PlayerEvent.NameFormat functionality.
  */
-@Mixin(PlayerEntity.class)
+@Mixin(Player.class)
 public class PlayerDisplayNameMixin {
 
     @Inject(method = "getDisplayName", at = @At("RETURN"), cancellable = true)
-    private void onGetDisplayName(CallbackInfoReturnable<Text> cir) {
-        PlayerEntity player = (PlayerEntity) (Object) this;
-        NbtCompound data = FakeNameData.getData(player);
+    private void onGetDisplayName(CallbackInfoReturnable<Component> cir) {
+        Player player = (Player) (Object) this;
+        CompoundTag data = FakeNameData.getData(player);
 
         if (data.contains("fakename")) {
-            String fakename = data.getString("fakename").orElse("");
-            cir.setReturnValue(Text.literal(fakename));
+            String fakename = data.getStringOr("fakename", "");
+            cir.setReturnValue(net.minecraft.network.chat.Component.literal(fakename));
         }
     }
 }
